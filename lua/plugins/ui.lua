b1a32171feb2da4356385bab175fff9d5bce6414
Manuel Lara <mlara@pinterest.com>
2024-05-14 12:26:38 -0600

    Lualine, new telescope shortcut, neogit to tab

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -49,4 +49,54 @@ return {
 			},
 		},
 	},
+	-- {
+	-- 	"nvim-lualine/lualine.nvim",
+	-- 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- 	config = function()
+	-- 		require("lualine").setup({})
+	-- 	end,
+	-- },
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.g.lualine_laststatus = vim.o.laststatus
+			if vim.fn.argc(-1) > 0 then
+				-- set an empty statusline till lualine loads
+				vim.o.statusline = " "
+			else
+				-- hide the statusline on the starter page
+				vim.o.laststatus = 0
+			end
+		end,
+		opts = function()
+			-- PERF: we don't need this lualine require madness 🤷
+			local lualine_require = require("lualine_require")
+			lualine_require.require = require
+
+			vim.o.laststatus = vim.g.lualine_laststatus
+
+			return {
+				options = {
+					theme = "auto",
+					globalstatus = true,
+					disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch" },
+					lualine_y = {
+						{ "progress", separator = " ", padding = { left = 1, right = 0 } },
+						{ "location", padding = { left = 0, right = 1 } },
+					},
+					lualine_z = {
+						function()
+							return " " .. os.date("%R")
+						end,
+					},
+				},
+				extensions = { "neo-tree", "lazy" },
+			}
+		end,
+	},
 }
